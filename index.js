cd60f907c419588b344759e7ca09a06a877523ea
BAUHAUS <Patrickfromgnomedk@gmail.com>
2025-03-28 10:08:54 +0100

    test2

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+let server;
 
-module.exports = app;
+if (require.main === module) {
+  server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+module.exports = { app, server };
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const request = require('supertest');
-const app = require('./index'); // Import the app
+const { app, server } = require('./index'); // Import both app and server
+
+afterAll(() => {
+  server.close(); // Close the server after tests finish
+});
 
 test('GET / should return Hello World!', async () => {
   const response = await request(app).get('/');
